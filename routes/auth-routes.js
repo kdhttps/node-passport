a111c93011fd303784f958bef34865c5e3fec313
kdhttps <kdhttps@gmail.com>
2020-11-03 19:59:09 +0530

    refactor

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -38,7 +38,7 @@ router.get('/inbound_saml', passport.authenticate('oidc-acr-passport-saml', {}))
 
 // openid-client
 // auth with oidc
-router.get('/openidclient', passport.authenticate('openid-client', { scope: 'openid profile' }))
+router.get('/openidclient', passport.authenticate('openid-client', { scope: 'openid profile email' }))
 
 // redirect(callback) uri for oidc
 router.get('/openidclient/redirect', passport.authenticate('openid-client'), (req, res) => {
