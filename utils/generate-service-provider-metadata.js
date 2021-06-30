91c584f5138477f0ceb77898fa40d8e7a9a54915
kdhttps <kdhttps@gmail.com>
2021-06-30 17:41:17 +0530

    generate metadata using multiple saml strategy

--- utils/generate-service-provider-metadata.js
+++ utils/generate-service-provider-metadata.js
@@ -1,42 +1,42 @@
 const fs = require('fs')
-const SAMLStrategy = require('../config/passport-setup').oPassportOIDCStrategy
+const SAMLStrategy = require('../config/passport-setup').oPassportSAMLStrategy
 
 // decryptionCert from your idp - /etc/certs/passport-sp.crt in Gluu CE Case
 const decryptionCertContent = `-----BEGIN CERTIFICATE-----
-MIIDXzCCAkcCFHizgWdN08Xx252haFX+HOxIO9IuMA0GCSqGSIb3DQEBCwUAMGwx
-CzAJBgNVBAYTAklOMQswCQYDVQQIDAJHSjEOMAwGA1UEBwwFU3VyYXQxDTALBgNV
-BAoMBEdsdXUxEjAQBgNVBAMMCWxvY2FsaG9zdDEdMBsGCSqGSIb3DQEJARYOa2ly
-YW5AZ2x1dS5vcmcwHhcNMjAwOTE1MTMyODI4WhcNMjEwOTE1MTMyODI4WjBsMQsw
-CQYDVQQGEwJJTjELMAkGA1UECAwCR0oxDjAMBgNVBAcMBVN1cmF0MQ0wCwYDVQQK
-DARHbHV1MRIwEAYDVQQDDAlsb2NhbGhvc3QxHTAbBgkqhkiG9w0BCQEWDmtpcmFu
-QGdsdXUub3JnMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs232wBVX
-dLiUAq+7Cj56Hof3N/jzC6pu/SCmu6eeOQ7p4mYIlK159SpcT26+enS3psnS5ygw
-QOyuY524JO7sTaFDjmfCyr56e/xWoYIw3QKQUhknS5ydQcyWwGaDd2Olq284mC+/
-iGM3xSV4RZ+ObkXFWNybmlPTudy64yLxaYD7LoqovSQ64xwUtjQlPgAPgSKHlw93
-Vv9u/cNRSkMnuLVRH1svsHNUasFUqvJr3KGljzfqXPc/kUYQ4oRFQFbkp9ZbYsG9
-vVypz7Vpact6bexyaZbD1tnp09P3q1TCFr0HDH19vG0/5PpvsVmht+dkMFw6l013
-YaM59rNZeEwvRQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAjt79vS882zHw+gOEA
-bfTjavBDv9DQX+JIL/+Z4Jyhs6wprH4LpZhrkNqPmR1V+3tFsGzdAzSjXeAVWygG
-feLUhv1slU2tDZeB466eFdicKPa8f58L1s+3QXtonZkJo09BvjuSq2vG8mkSDzI9
-TtInea7dkYpYnIy34mUvJlAoIucUIMFneglXsmyI2adnC9hj51+CzATOe3NUI9qM
-QK6grL9Ih0SMgBYBLkxdoX6ht/xW86tKvBF23EaVFWLa28psKXtk0xvP4opmh/Pk
-gBZ7h20BtJgTFSRP1T6kjXqqojVXcctrl2SpLWKEAd1SbRd8iXVNRTdmYHTgKuCn
-3NM6
+MIIDWTCCAkECFEn2BnVdFe6iwZU8UwbUd+/JfReFMA0GCSqGSIb3DQEBCwUAMGkx
+CzAJBgNVBAYTAklOMQswCQYDVQQIDAJHSjELMAkGA1UEBwwCU1UxDTALBgNVBAoM
+BGdsdXUxEjAQBgNVBAMMCWxvY2FsaG9zdDEdMBsGCSqGSIb3DQEJARYOa2lyYW5A
+Z2x1dS5vcmcwHhcNMjEwMzA0MTgyMjIwWhcNMjIwMzA0MTgyMjIwWjBpMQswCQYD
+VQQGEwJJTjELMAkGA1UECAwCR0oxCzAJBgNVBAcMAlNVMQ0wCwYDVQQKDARnbHV1
+MRIwEAYDVQQDDAlsb2NhbGhvc3QxHTAbBgkqhkiG9w0BCQEWDmtpcmFuQGdsdXUu
+b3JnMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx2d+z6yp4gsQBRBh
+4Ux/7HzV6DjFMfwCQbxw5VUyYJWyJ3sFJ61jKBYKZA//h2mVsHmP2Q1SF6Ww0Sfa
+7uuEzudyCaL405oUJyusbCSuGRNPMCWX8sU0Xhm0XkXtkGCKbnRBbexUl8ieEW+t
+GvBytXk502kW+qq5tRa2y/q6+owzAqZ/IHoNUJSDtyqKS+6zOyr5Hndv389Aqprj
+oLcmPFMFyU9IBZ9clongyHb3gwQM1W8DewlEKJL7TI7LDHIhAHZJEUIcpuGFtAsW
+zUoqfbaFWcZ+xqRBSXp37CDfAffQjpnKPdLm9RmcX4nOPWqTUhlJdL3lRwAhL87W
+tJ/gRwIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQCDU0M3jV/fR00/PgxH/kMwOrC5
+MopRLAbX4CmFWLRZj/hTqk3oIEz0MwvyE4Z4+GdMhTy9hajnu1hh8bXRkjjq5BYN
+PH4Yg8pJrYUFibbA9V21nekjILR8QDiNBboNPlfAdbRclm5KQy/DpDxNDRVPbY49
+eqwA9ILfrqy1Qp3LR146oNVcaJ2SSSM+kyT77KRiU8vXgGmYsfYtf9lAFawc7eVZ
+w7VVxQ/khMbgrhSFCaB35kUGcgkjvjQsANIHV8TQPrgWKhnwZ9oy4yj6qt/LjMS8
+crz/sh77342B2USUYB5Ujz3sJh0i2Wk5z/rqlp/rq8y0txXEt6QrRG/LXCAg
 -----END CERTIFICATE-----`
-
-// signingCert - the cert from url https://<your.idp.com>/idp/shibboleth endpoint in side `signing` > `ds:X509Certificate`
-const signingCertContent = 'MIIDZzCCAk8CFBEpsZWJPn11KnqUTQCbTVRo/e24MA0GCSqGSIb3DQEBCwUAMHAx CzAJBgNVBAYTAklOMQswCQYDVQQIDAJHSjEOMAwGA1UEBwwFU3VyYXQxDTALBgNV BAoMBEdsdXUxFjAUBgNVBAMMDWdsdXUubWFsaS5vcmcxHTAbBgkqhkiG9w0BCQEW DmtpcmFuQGdsdXUub3JnMB4XDTIwMDkxNTEzMjgyN1oXDTIxMDkxNTEzMjgyN1ow cDELMAkGA1UEBhMCSU4xCzAJBgNVBAgMAkdKMQ4wDAYDVQQHDAVTdXJhdDENMAsG A1UECgwER2x1dTEWMBQGA1UEAwwNZ2x1dS5tYWxpLm9yZzEdMBsGCSqGSIb3DQEJ ARYOa2lyYW5AZ2x1dS5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB AQDGTkW9GGgd0U+2UEtzREb7oLj/O3XwavQClNTzY9VOyN0E8wmHl9BVfRWG0NoH zyoRDg7qikg8Z8OjIkuO70lmnW6A5C5441nw6FtBs7aRYProjMHEo7Tqihq+FtPW Pk2zEaRhuYsWqS27zuqz/rkoASkFDlvi4zr+T9MDf3i8Fo1l6+ZJqDzOBt4y98vG X6PjDRnpNMmcGBW72Jccl2QNeKH4XYVjVMQ/d/vXZL4aLmdNX81qkZUXyi881hwg XOJ9dSTCIPUqUUSqmlKWyltsqa35NbtqJwD10nRsxYCO09BleK2A3MBl9IzFwDY/ vw0Rb3ziGnMQ55OpQsPsXKTdAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAL/PtrPT Dlvs0zOUW7jmyO0snHmVXJYsmEd7HkWf40t9YEtVm8KmdzkoBwzj3pTjDmSS3i56 QDxWRBFytDclLaugWSC15CTSCiywuuToIQSyzQwU7RxFDBKZSeP7KqaOsdgN0WL9 NXi1DZQ93VKwfPq7uW5evRqQvMQz/38B/PUvai0gBbGF3B3N8REaWpbu+vNFsICO 0WX5kp1Cwp7YoWU1K/+03XKoFWzfzwWebgNlbhmHTWfJ1yXAxPkp3aEURxZOB90g cJi5f1HAzbAyaz6QJp5ta4iYZRnGITW82xtf0qC1oxH8e6sfrwLyIfPHxlCAaq6k pt5YQWUfMZKxlW0='
 
 // metafile name
 const metaFile = `${__dirname}/local.xml`
 
 async function generateMetadata () {
   try {
-    const metaContent = await SAMLStrategy.generateServiceProviderMetadata(decryptionCertContent, signingCertContent)
-    console.log(metaContent)
-    console.log(metaFile)
-    await fs.openSync(metaFile, 'w')
-    await fs.writeFileSync(metaFile, metaContent)
+    SAMLStrategy.generateServiceProviderMetadata({ originalUrl: '/auth/saml1/redirect' }, decryptionCertContent, null, async function (err, metaContent) {
+      if (err) {
+        console.log(err)
+      }
+      console.log(metaContent)
+      console.log(metaFile)
+      await fs.openSync(metaFile, 'w')
+      await fs.writeFileSync(metaFile, metaContent)
+    })
   } catch (e) {
     console.log(e)
   }
